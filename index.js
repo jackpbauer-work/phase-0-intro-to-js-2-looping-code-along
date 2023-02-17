b5808b927bf17bce035056b22833c8f23b7e390c
Jack Bauer <jackbauer.work@gmail.com>
2023-02-17 12:48:12 -0500

    looping

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Code your solutions in this file
+for (let age = 30; age < 40; age++) {
+    console.log(`I'm ${age} years old. Happy birthday to me!`);
+}
+const myGifts = ["teddy bear", "drone", "doll"];
+function wrapGifts(gifts) {
+    for (let i = 0; i < gifts.length; i++) {
+        console.log(`Wrapped ${gifts[i]} and added a bow!`);
+    }
+    return gifts;
+}
+
+console.log(wrapGifts(myGifts))
+
+
+function writeCards(names, event) {
+    const messages = [];
+    for (let i = 0; i < names.length; i++) {
+        const message = `Thank you, ${names[i]}, for the wonderful ${event} gift!`
+        messages.push(message)
+    }
+        return messages;
+}
+function countDown (startingNumber) {
+    while (startingNumber >= 0){
+        console.log(startingNumber);
+        startingNumber = startingNumber - 1;
+    }
+    
+}
